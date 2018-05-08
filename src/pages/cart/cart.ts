a750535e34f194b788252dac134d68363fd255ce
Naveen <naveen241193@gmail.com>
2018-05-08 18:17:40 +0530

    asdfasdf

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -3,22 +3,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-cart',
-  templateUrl: 'cart.html'
+    selector: 'page-cart',
+    templateUrl: 'cart.html'
 })
 
-export class CartPage{
-    cartItems: any;
+export class CartPage {
+    cartItems: any = {};
     args: any = 0;
-    constructor(public navParams: NavParams, public navCtrl: NavController){
+    coupon: any;
+    cart: any = 0;
+    tax: any = 23.45;
+    total: any = 23;
+    constructor(public navParams: NavParams, public navCtrl: NavController) {
         this.cartItems = navParams.data.items;
-        console.log(navParams.data)
     }
 
-    ionViewDidLoad(){
+    ionViewDidLoad() {
     }
 
-    backButtonClick(){
+    backButtonClick() {
         this.navCtrl.pop();
+    }
+
+    add(item) {
+        this.cartItems.forEach((element,index) => {
+            if(item === element.title){
+                this.cartItems[index].quantity++;
+            }
+        });
+        this.cartCount();
+    }
+
+    remove(item) {
+        this.cartItems.forEach((element,index) => {
+            if(item === element.title && element.quantity > 0){
+                this.cartItems[index].quantity--;
+            }
+        });
+        this.cartCount();
+    }
+
+    cartCount() {
+        this.cart = 0;
+        this.cartItems.forEach(element => {
+            this.cart = element.quantity + this.cart;
+        });
+        if (this.cart > 99)
+            this.cart = '99+';
     }
 }
