e653e56de7c4f162af3377f9af1b7c5b0ccce890
karun kumar <karun@b2bitsolutions.com>
2018-05-09 21:05:54 +0530

    34324

--- src/pages/rider-delivery/rider-delivery.module.ts
+++ src/pages/rider-delivery/rider-delivery.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { RiderDeliveryPage } from './rider-delivery';
+import { ExpandableHeader } from '../../components/expandable-header';
 
 @NgModule({
     declarations: [
-        RiderDeliveryPage
+        RiderDeliveryPage,
+        ExpandableHeader
     ],
     imports: [
         IonicPageModule.forChild(RiderDeliveryPage)
--- src/pages/rider-delivery/rider-delivery.ts
+++ src/pages/rider-delivery/rider-delivery.ts
@@ -9,5 +9,65 @@ import { IonicPage, NavController } from 'ionic-angular';
 
 export class RiderDeliveryPage {
     displayType: any = 'today';
+    today: any = [
+        {
+            order: 'PO# 1100011',
+            status: 'ready',
+            branch: 'Little India',
+            quantity: '80',
+            vessels: '3'
+        },
+        {
+            order: 'PO# 1100012',
+            status: 'packing',
+            branch: 'Fancy',
+            quantity: '60',
+            vessels: '5'
+        },
+        {
+            order: 'PO# 1100013',
+            status: 'packing',
+            branch: 'Little India',
+            quantity: '100',
+            vessels: '7'
+        },
+        {
+            order: 'PO# 1100014',
+            status: 'packing',
+            branch: 'Fine Biriyani',
+            quantity: '120',
+            vessels: '10'
+        }
+    ];
+    completed: any = [
+        {
+            order: 'PO# 1100009',
+            status: 'completed',
+            branch: 'Fine Biriyani',
+            quantity: '120',
+            vessels: '10'
+        },
+        {
+            order: 'PO# 1100008',
+            status: 'completed',
+            branch: 'Little India',
+            quantity: '80',
+            vessels: '3'
+        },
+        {
+            order: 'PO# 1100007',
+            status: 'completed',
+            branch: 'Fancy',
+            quantity: '60',
+            vessels: '5'
+        },
+        {
+            order: 'PO# 1100006',
+            status: 'completed',
+            branch: 'Little India',
+            quantity: '100',
+            vessels: '7'
+        }
+    ]
     constructor(public navCtrl: NavController){}
 }
